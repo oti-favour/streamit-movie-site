db017dddd625e4072029f440487b5ac86b3bb0aa
oti-favour <favourgraceoti@gmail.com>
2024-11-19 12:37:24 +0100

    new features

--- app/[media_type]/[id]/page.tsx
+++ app/[media_type]/[id]/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
+// Function to fetch details based on media_type and id
 async function fetchDetails(media_type: string, id: string) {
   const response = await fetch(
     `https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
   );
 
+  // If the response isn't ok, return null
   if (!response.ok) {
     return null;
   }
@@ -13,20 +15,24 @@ async function fetchDetails(media_type: string, id: string) {
   return response.json();
 }
 
+// Next.js Page Component
 const DetailsPage = async ({
   params,
 }: {
   params: { media_type: string; id: string };
 }) => {
+  // Destructure media_type and id from params
   const { media_type, id } = params;
 
-  // Ensure we're awaiting the fetchDetails correctly
+  // Fetch data using the provided media_type and id
   const data = await fetchDetails(media_type, id);
 
+  // If no data is found, trigger a 404 page
   if (!data) {
     notFound();
   }
 
+  // Return the details page with data
   return (
     <div className="p-8 px-16 text-white">
       <h1 className="text-3xl font-bold">{data.title || data.name}</h1>
