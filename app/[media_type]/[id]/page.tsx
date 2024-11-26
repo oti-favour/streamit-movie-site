38488f7ba7a351ac7d3b049408f78e7354d0fcb7
oti-favour <favourgraceoti@gmail.com>
2024-11-26 21:58:56 +0100

    cor

--- app/[media_type]/[id]/page.tsx
+++ app/[media_type]/[id]/page.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import TrailerButton from "@/components/common/TrailerButton";
 import Image from "next/image";
--- components/common/TrailerButton.tsx
+++ components/common/TrailerButton.tsx
@@ -1,5 +1,6 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import React from "react";
 import { AiOutlinePlayCircle } from "react-icons/ai";
 import { useRouter } from "next/navigation";
